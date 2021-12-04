7cdb0e65e6c31e245f566396b14d81a48f167cb2
Choliq <choliq.24@students.amikom.ac.id>
2021-12-04 14:55:10 +0700

    tes 2

---
+++ NewRepo/NewRepo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NewRepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Pemrograman Lanjut S1 Informatika");
+        }
+    }
+}
